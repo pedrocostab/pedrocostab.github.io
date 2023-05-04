91ebe797399fd64ad95ae7aa3e4847ff4e2062bc
pedrocostab <40375460+pedrocostab@users.noreply.github.com>
2023-05-04 17:04:59 -0300

    Added Swipper at portfolio session

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,18 @@ modalClose.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
